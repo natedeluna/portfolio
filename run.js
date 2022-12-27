1ce38cd5f4dd3e0065333da318426e2dd13bbf80
natedeluna <nate.d.luna@gmail.com>
2022-12-27 16:49:54 -0600

    performace optimization

--- run.js
+++ run.js
@@ -1,9 +1,9 @@
 
 
 
-    let video = document.querySelector('.video');
+    // let video = document.querySelector('.video');
 
-    setTimeout(() =>{video.firstElementChild.play()},1300)
+    // setTimeout(() =>{video.firstElementChild.play()},1300)
 
     // let tl = gsap.timeline({delay: 5.8});
     //     tl.fromTo(video,{duration: 2.5, clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)"}, { duration: 1, clipPath: "polygon(51% 46%, 54% 46%, 46% 61%, 43% 61%)"});
@@ -15,73 +15,91 @@
     //     tl.add(() => {animOnScroll();
     //         drawOnScroll();}, ">-1.6")
     // tl.play();
+
+    // window.addEventListener('mousemove', (e) => {
+    //     let radian = (Math.atan2(e.y, e.x) * (Math.PI * 36)*.001);
+    //     gradientChange.style.background = `conic-gradient(from ${radian}turn at 0% 0%, #eca6a4, 48deg,  #a7bac2, 150deg, #c4ddff)`
+    // })
+    // function calcRadian () {
+    //     let radian = (Math.atan2(e.y, e.x) * (Math.PI * 36)*.001);
+    // }
     
-    window.addEventListener('mousemove', (e) => {
-        let radian = (Math.atan2(e.y, e.x) * (360 / Math.PI)*.001);
-        document.querySelector(':root').style.setProperty('--gradient', `conic-gradient(from ${radian}turn at 0% 0%, #eca6a4, 48deg,  #a7bac2, 150deg, #c4ddff)`)
+    var x = 0
+    var y = 0
+    let gradientChange = document.querySelector('.background')
+
+    
+    document.addEventListener('mousemove', (e) => {
+        x = e.x;
+        y = e.y
     })
-
-
-    window.addEventListener('wheel', animOnScroll)
-    window.addEventListener('wheel', drawOnScroll)
+    
+    window.requestAnimationFrame(function updateGradient() {
+        let radian = (Math.atan2(y, x) * (360 / Math.PI)*.001);
+        gradientChange.style.background = `conic-gradient(from ${radian}turn at 0% 0%, #eca6a4, 48deg,  #a7bac2, 150deg, #c4ddff)`
+        window.requestAnimationFrame(updateGradient);
+    })
+    
+    // drawOnScroll();
+    // animOnScroll();
+    // window.addEventListener('wheel', animOnScroll)
+    // window.addEventListener('wheel', drawOnScroll)
     
  
 
-    function animOnScroll() {
-        var elit = document.querySelectorAll('.fade-down');
-        for(let i = 0; i< elit.length; i++) {
-            if (elit[i].getBoundingClientRect().top <= 5000) {
-                elit[i].classList.remove('fade-down');
-                elit[i].classList.add('fade-down-active');
-                var animActive = document.querySelectorAll('.fade-down-active');
-                gsap.fromTo(animActive[animActive.length-1], {y: -28, opacity: 0},{y: -3, opacity: 1, duration: 1})
+    // function animOnScroll () {
+    //     var elit = document.querySelectorAll('.fade-down');
+    //     for(let i = 0; i< elit.length; i++) {
+    //         if (elit[i].getBoundingClientRect().top <= 5000) {
+    //             elit[i].classList.remove('fade-down');
+    //             elit[i].classList.add('fade-down-active');
+    //             var animActive = document.querySelectorAll('.fade-down-active');
+    //             gsap.fromTo(animActive[animActive.length-1], {y: -28, opacity: 0},{y: -3, opacity: 1, duration: 1})
         
-                console.log(elit.length)
-            }
-        }
-        if (elit.length === 0){
-            window.removeEventListener('wheel', animOnScroll)
-            console.log('removed')
-        }
-    }
+    //             console.log(elit.length)
+    //         }
+    //     }
+    //     if (elit.length === 0){
+    //         window.removeEventListener('wheel', animOnScroll)
+    //         console.log('removed')
+    //     }
+    // }
 
-    function drawOnScroll () {
-        let svgEl = document.querySelectorAll('.setVB')
-        for (let v=0; v<svgEl.length; v++) {
-            svgEl[v].setAttribute("viewBox", `0 0 ${window.innerWidth * .5} 2`)
-            svgEl[v].firstElementChild.setAttribute("d", `M-31 1H${window.innerWidth * .5}`)
-            console.log(svgEl[v])
-        }
+    // function drawOnScroll () {
+    //     let svgEl = document.querySelectorAll('.setVB')
+    //     for (let v=0; v<svgEl.length; v++) {
+    //         svgEl[v].setAttribute("viewBox", `0 0 ${window.innerWidth * 1.5} 2`)
+    //         svgEl[v].firstElementChild.setAttribute("d", `M-31 1H${window.innerWidth * 1.5}`)
+    //         console.log(svgEl[v])
+    //     }
 
-        var draw = document.querySelectorAll('.svg-draw');
+    //     var draw = document.querySelectorAll('.svg-draw');
 
-        for(let n = 0; n< draw.length; n++) {
-            if (draw[n].getBoundingClientRect().top <= 2000) {
-                draw[n].style.animation="draw 3s cubic-bezier(.08,.47,.07,.81) forwards"
-                setTimeout(() => {
-                    draw[n].classList.remove('svg-draw')
-                },4000)
-            }
-        }
-        if (draw.length === 0) {
-            window.removeEventListener('wheel', drawOnScroll)
-            console.log('done')
-        }
-    }
+    //     for(let n = 0; n< draw.length; n++) {
+    //         if (draw[n].getBoundingClientRect().top <= 2000) {
+    //             gsap.from(draw[n], {strokeDashoffset: "2000", strokeDasharray: "2000"})
+    //             gsap.to(draw[n], {duration: 4, strokeDashoffset: "0", ease: "power4.out"})
+    //             // draw[n].style.animation="draw 5s cubic-bezier(.08,.47,.07,.81) forwards"
+    //         }
+    //     }
+    // }
 
+    // let headerVis = document.getElementsByClassName("header-name-v")[0]
+    // let indexMI = document.getElementById("index");
+    // let msgMI = document.getElementById("msg");
 
-    document.getElementById("index").addEventListener('mouseover', () => {
-        document.getElementsByClassName("header-name-v")[0].style.cssText += "background-color: red; transform: scale(1.7); width: 20px; transition: all 1s;"
-    })
-    document.getElementById("msg").addEventListener('mouseover', () => {
-        document.getElementsByClassName("header-name-v")[0].style.cssText += "background-color: blue; transform: scale(1.7); width: 20px; transition: all 1s;"
-    })
+    // indexMI.addEventListener('mouseover', () => {
+    //     headerVis.style.cssText += "background-color: red; transform: scale(1.7); width: 20px; transition: all .5s;"
+    // })
+    // msgMI.addEventListener('mouseover', () => {
+    //     headerVis.style.cssText += "background-color: blue; transform: scale(1.7); width: 20px; transition: all .5s;"
+    // })
     
     
-    document.getElementById("index").addEventListener('mouseleave', () => {
-        document.getElementsByClassName("header-name-v")[0].style.cssText = "background-color: black; transform: scale(1); transition: all 1s;"
-    })
-    document.getElementById("msg").addEventListener('mouseleave', () => {
-        document.getElementsByClassName("header-name-v")[0].style.cssText = "background-color: black; transform: scale(1); transition: all 1s;"
-    })
+    // indexMI.addEventListener('mouseleave', () => {
+    //     headerVis.style.cssText = "background-color: black; transform: scale(1); transition: all .5s;"
+    // })
+    // msgMI.addEventListener('mouseleave', () => {
+    //     headerVis.style.cssText = "background-color: black; transform: scale(1); transition: all .5s;"
+    // })
 
